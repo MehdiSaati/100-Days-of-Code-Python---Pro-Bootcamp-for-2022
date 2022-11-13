aca4c760b0593a4ef289f97aa9747764698bc46c
mehdi <m@gmail.com>
2022-11-13 14:27:33 +0330

    add take photo to rollercoaster project

--- day03/rollercoaster.py
+++ day03/rollercoaster.py
@@ -1,16 +1,28 @@
 # rollercoaster project
 print(" Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 # check user height
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     # check age for pay
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+    
+    # Check take photo 
+    wants_photo = input("Do you want a photo> Y or N. ").lower()
+    if wants_photo == "y":
+       bill = bill + 3
+    
+    print(f"Your final bill is ${bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
