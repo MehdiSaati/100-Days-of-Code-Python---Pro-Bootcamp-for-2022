126af1654f5a30374750bee8e88fbdaf91502012
mehdi <m@gmail.com>
2022-11-14 12:03:52 +0330

    Who's Paying?

---
+++ day04/who_paying.py
@@ -0,0 +1,17 @@
+# Who's Paying?
+import random
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Spliting string method
+namesASCSV = input("Give me everybody\'s name, seperated by a coma. ")
+names = namesASCSV.split(", ")
+
+# Get the total number of items in list.
+num_items = len(names)
+
+# Generate random number between 0 the last item in list
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+print(f"{person_who_will_pay} is going to by the meal today" )
+ 
