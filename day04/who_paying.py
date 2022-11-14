256e47828281023174262f25a45a585543b74137
mehdi <m@gmail.com>
2022-11-14 12:08:33 +0330

    update code by random choice

--- day04/who_paying.py
+++ day04/who_paying.py
@@ -7,11 +7,11 @@ random.seed(test_seed)
 namesASCSV = input("Give me everybody\'s name, seperated by a coma. ")
 names = namesASCSV.split(", ")
 
-# Get the total number of items in list.
-num_items = len(names)
+# # Get the total number of items in list.
+# num_items = len(names)
+# # Generate random number between 0 the last item in list
+# random_choice = random.randint(0, num_items - 1)
+# person_who_will_pay = names[random_choice]
+person_who_will_pay = random.choice(names)
 
-# Generate random number between 0 the last item in list
-random_choice = random.randint(0, num_items - 1)
-person_who_will_pay = names[random_choice]
 print(f"{person_who_will_pay} is going to by the meal today" )
- 
