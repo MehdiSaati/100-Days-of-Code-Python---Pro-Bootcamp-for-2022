c49ee937c706548074e11616500f3855b037810c
mehdi <m@gmail.com>
2022-11-18 11:05:03 +0330

    functions whit inputs

---
+++ day08/functions_whit_inputs.py
@@ -0,0 +1,15 @@
+# Simple Function
+# Create a function called greet().
+
+# def greet():
+#     print("Hello")
+#     print("How do you do?")
+#     print("Isn't the weather nice today?")
+# greet()
+
+# Function that allows for input
+def greet_whit_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+greet_whit_name("mehdi")
+ 
