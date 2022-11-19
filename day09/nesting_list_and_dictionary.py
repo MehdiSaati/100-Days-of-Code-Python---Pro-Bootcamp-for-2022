b915e849978235136dd5375f9cbcbcbc64386751
mehdi <m@gmail.com>
2022-11-19 13:55:18 +0330

    update nesting list and dictionary

--- day09/nesting_list_and_dictionary.py
+++ day09/nesting_list_and_dictionary.py
@@ -18,13 +18,26 @@ travel_log = {
 travel_log = [
     {
         "country": "France", 
-        "cities_visited":["Paris", "Lille", "Dijon"],
-        "total_visites": 12
+        "total_visites": 12,
+        "cities_visited":["Paris", "Lille", "Dijon"]
+        
     },
     {
         "country":"Germany",
-        "cities_visited":["Berline", "Hamburg", "Stutgart"],
-        "total_visits": 5
+        "total_visits": 5,
+        "cities_visited":["Berline", "Hamburg", "Stutgart"]
+        
     }
 ]
+print(travel_log)
+# add new country
+def add_new_country(country_visited, times_visited, cities_visited):
+    new_country = {}
+    new_country["country"] = country_visited
+    new_country["total_visites"] = times_visited
+    new_country["cities_visited"] = cities_visited
+    travel_log.append(new_country)
+
+
+add_new_country("Russia", 2, ["Moscow","Saint"])
 print(travel_log)
