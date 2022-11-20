4ee28cf713e5c4590cbcc5ad5d1c06759cfc78bd
mehdi <m@gmail.com>
2022-11-20 13:28:12 +0330

    Create Day in Month Program

---
+++ day10/days_in_month.py
@@ -0,0 +1,27 @@
+# Day in Month
+
+# Leap year Function
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False     
+        else:
+            return True
+    else:
+        return False
+def days_in_month(year, month):
+    if month > 12 or month < 1:
+        return "Invaild month"
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
+
+year = int(input("Enter a year : "))
+month = int(input("Enter a month :"))
+days = days_in_month(year, month)
+print(days)
+input(">")
