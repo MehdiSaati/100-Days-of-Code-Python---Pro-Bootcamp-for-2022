5d8c8a3892c5858b64ac0948d8bc96376d6f9715
mehdi <m@gmail.com>
2022-11-21 12:50:45 +0330

    Update Calculator by loop

--- day10/main.py
+++ day10/main.py
@@ -19,21 +19,26 @@ operations = {
     "*": multiply,
     "/": divide
 }
+def clculator():
+    n1 = int(input("What's the first number: "))
 
-n1 = int(input("What's the first number: "))
+    for symbol in operations:
+        print(symbol)
 
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from line above: ")
-n2 = int(input("What's the second number: "))
-calculation_function = operations[operation_symbol]
-answer = calculation_function(n1, n2)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        n2 = int(input("What's the next number: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(n1, n2)
 
-print(f"{n1} {operation_symbol} {n2} = {answer}")
- 
-operation_symbol = input("Pick another operaton: ")
-n3 = int(input("What's the next number? : "))
-calculation_function = operations[operation_symbol]
-answer2 = calculation_function(answer, n3)
+        print(f"{n1} {operation_symbol} {n2} = {answer}")
 
-print(f"{answer} {operation_symbol} {n3} = {answer2}")
+        con = input(f"Type 'y' to continue calcuting with {answer}, or type 'n' to start a new calculation: ")
+        if con == "y":
+            n1 = answer
+        else:
+            should_continue = False
+            clculator()
+
+clculator()
