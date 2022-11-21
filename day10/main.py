d6dc3ef22be0dff41c6e420b5ba08ee4b1da7a05
mehdi <m@gmail.com>
2022-11-21 12:01:48 +0330

    Update Calculator

--- day10/main.py
+++ day10/main.py
@@ -24,9 +24,16 @@ n1 = int(input("What's the first number: "))
 
 for symbol in operations:
     print(symbol)
-operation_symbol = input("Pick an operation from line aboe: ")
+operation_symbol = input("Pick an operation from line above: ")
 n2 = int(input("What's the second number: "))
 calculation_function = operations[operation_symbol]
 answer = calculation_function(n1, n2)
+
 print(f"{n1} {operation_symbol} {n2} = {answer}")
  
+operation_symbol = input("Pick another operaton: ")
+n3 = int(input("What's the next number? : "))
+calculation_function = operations[operation_symbol]
+answer2 = calculation_function(answer, n3)
+
+print(f"{answer} {operation_symbol} {n3} = {answer2}")
