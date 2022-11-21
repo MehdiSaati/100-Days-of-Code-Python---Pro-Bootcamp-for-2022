a5468aab8e5cc84acd892e3f87c538baca11d40f
mehdi <m@gmail.com>
2022-11-21 13:03:47 +0330

    Update and fix bug Calculator

--- day10/main.py
+++ day10/main.py
@@ -1,6 +1,6 @@
 # Calculator
 from art import logo
-print(logo)
+
 # Add
 def add(n1, n2):
     return n1+n2
@@ -20,7 +20,8 @@ operations = {
     "/": divide
 }
 def clculator():
-    n1 = int(input("What's the first number: "))
+    print(logo)
+    n1 = float(input("What's the first number: "))
 
     for symbol in operations:
         print(symbol)
@@ -28,7 +29,7 @@ def clculator():
     should_continue = True
     while should_continue:
         operation_symbol = input("Pick an operation: ")
-        n2 = int(input("What's the next number: "))
+        n2 = float(input("What's the next number: "))
         calculation_function = operations[operation_symbol]
         answer = calculation_function(n1, n2)
 
