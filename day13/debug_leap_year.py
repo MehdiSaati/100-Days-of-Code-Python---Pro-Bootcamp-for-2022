5a98d2109648f3a7fb38486a6dc1adcfd9ea4fac
mehdi <m@gmail.com>
2022-11-26 11:05:47 +0330

    Debug Leap Year

---
+++ day13/debug_leap_year.py
@@ -0,0 +1,15 @@
+# Debug Leap Year
+#year = input("Which year do you want to check? ")
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year.")
+input(">")
