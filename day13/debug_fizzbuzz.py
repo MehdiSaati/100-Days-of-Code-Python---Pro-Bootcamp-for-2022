b3df5a01a53543c8bcf8331067c1ea8abe7f0a98
mehdi <m@gmail.com>
2022-11-26 11:21:06 +0330

    Debug FizzBuzz

---
+++ day13/debug_fizzbuzz.py
@@ -0,0 +1,13 @@
+# Debug FizzBuzz 
+for number in range(1, 101):
+   # if number % 3 == 0 or number % 5 == 0:
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+       # print([number])
+       print(number)
+ 
