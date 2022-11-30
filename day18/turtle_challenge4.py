e1b218e2112920ed7198374b1f7ef26be1d7b659
mehdi <m@gmail.com>
2022-12-01 01:26:18 +0330

    Update Turtle Challenge 4 Draw a Random Walk

--- day18/turtle_challenge4.py
+++ day18/turtle_challenge4.py
@@ -1,17 +1,27 @@
 # Turtle Challenge 4 Draw a Random Walk
 from turtle import Turtle, Screen
 import random
-color = ["aquamarine", "CornflowerBlue", "IndianRed", "DarkOrchid", "green", "red", "yellow", "black" ,"DeepSkyBlue", "LightSeaGreen", "SeaGreen"]
+# color = ["aquamarine", "CornflowerBlue", "IndianRed", "DarkOrchid", "green", "red", "yellow", "black" ,"DeepSkyBlue", "LightSeaGreen", "SeaGreen"]
 directions = [0, 90, 180, 270]
 mehdi_the_turtle = Turtle()
 mehdi_the_turtle.pensize(5)
 mehdi_the_turtle.speed("fastest")
+mehdi_the_turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rand_color = (r, g, b)
+    return rand_color
+
 def random_walk(angle):
     mehdi_the_turtle.forward(10)
     mehdi_the_turtle.setheading(angle)
 
 while True:
-    mehdi_the_turtle.color(random.choice(color))
+    # mehdi_the_turtle.color(random.choice(color))
+    mehdi_the_turtle.color(random_color())
     rand_angle = random.choice(directions)
     random_walk(rand_angle)
 
