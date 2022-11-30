65cd048091d8efa81a7676c3531d861e754334a1
mehdi <m@gmail.com>
2022-11-30 21:44:44 +0330

    Create Turtle Challenge 2 - Draw Square by penup

---
+++ day18/turtle_challenge2.py
@@ -0,0 +1,34 @@
+# Turtle Challenge - Draw Square
+from turtle import Turtle, Screen
+
+
+mehdi_the_turtle = Turtle()
+
+# draw line by black and white color
+
+# def draw_line():
+#     mehdi_the_turtle.color("red")
+#     mehdi_the_turtle.forward(5)
+#     mehdi_the_turtle.color("white")
+#     mehdi_the_turtle.forward(5)
+ 
+# for _ in range(4):
+#     for _ in range(10):
+#        draw_line()
+#     mehdi_the_turtle.right(90)
+#     draw_line()
+
+# draw by pen up
+for _ in range(15):
+    mehdi_the_turtle.forward(10)
+    mehdi_the_turtle.penup()
+    mehdi_the_turtle.forward(10)
+    mehdi_the_turtle.pendown()
+
+
+
+
+my_screen = Screen()
+my_screen.exitonclick()
+
+input(">")
