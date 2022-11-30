ab574706590c7fc5018f56fe04df78af00d76610
mehdi <m@gmail.com>
2022-11-30 21:07:29 +0330

    Create Turtle Challenge - Draw Square

---
+++ day18/turtle_challenge1.py
@@ -0,0 +1,31 @@
+# Turtle Challenge - Draw Square
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("red")
+
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+my_screen = Screen()
+my_screen.exitonclick()
+
+input(">")
