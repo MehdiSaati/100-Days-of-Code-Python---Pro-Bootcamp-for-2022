8d6b7ec0de774d400ce694afd782a8c98ff20c56
mehdi <m@gmail.com>
2022-12-01 12:12:47 +0330

    Create Python Higher Order Functions & Event Listeners

---
+++ day19/main.py
@@ -0,0 +1,12 @@
+# Python Higher Order Functions & Event Listeners
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+screen.listen()
+screen.onkey(key="space", fun=move_forwards)
+screen.exitonclick()
