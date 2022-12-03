2bc6ba4c43b5312f577e676f3f045a5968b742f4
mehdi <m@gmail.com>
2022-12-03 19:53:24 +0330

    Build the Snake Game Part 2

---
+++ day21/food.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.shapesize(stretch_len=1, stretch_wid=1)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
