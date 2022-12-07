b90738465e2bcee3095e567af052190917cec00c
mehdi <m@gmail.com>
2022-12-07 13:06:30 +0330

    DataFrames & Series Working with Rows & Columns

--- day25/main.py
+++ day25/main.py
@@ -18,6 +18,50 @@ os.chdir("/Users/mahan/Desktop/100DaysOfCodePython-master/day25")
 import pandas
     
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
- 
-input(">")
+# print(data["temp"])
+
+#convert to dictionary
+data_dict = data.to_dict()
+print(data_dict)
+
+#convert to list
+temp_list = data["temp"].to_list()
+
+# average columns - temp
+
+# average_temp = sum(temp_list) / len(temp_list)
+# print(average_temp)
+print(data["temp"].mean())
+
+#Max in columns by series method
+print(data["temp"].max())
+
+#Get Data in columns
+print(data["condition"])
+print(data.condition)
+
+# Get Data in Row
+print(data[data["day"] == "Monday"])
+
+
+# Get maximum temp row data
+max_temp = data.temp.max()
+print(data[data.temp == max_temp])
+
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+# convert celsius to fahrenheit
+monday_Celsius = int(monday.temp)
+monday_Fahrenheit = 9.0/5.0 * monday_Celsius + 32
+print(f"Temperature:{monday_Celsius} Celsius , {monday_Fahrenheit} F") 
+
+# Create dataframe from scratch
+data_student_dict ={
+    "students": ["Amy", "James", "Angela"],
+    "score": [76, 56, 65]
+}
+data = pandas.DataFrame(data_student_dict)
+print(data)
+data.to_csv("new_data.csv")
