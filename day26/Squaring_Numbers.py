9ac99105c92af98cbbc09c0b994716fa562988c0
mehdi <m@gmail.com>
2022-12-08 10:35:00 +0330

    Create Squared Numbers By List Comprehension

---
+++ day26/Squaring_Numbers.py
@@ -0,0 +1,8 @@
+# Squared Numbers By List Comprehension
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+# new_list = [new_item for item in list]
+# squared number : nummber * number or number**2
+squared_numbers = [num**2 for num in numbers]
+
+print(squared_numbers)
