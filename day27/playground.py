61a8ebb61086dc9c9e98ae2da99030cb2d3f6712
mehdi <m@gmail.com>
2022-12-10 10:43:17 +0330

    kwargs Many Keyword Arguments

--- day27/playground.py
+++ day27/playground.py
@@ -10,3 +10,30 @@ def add(*args):
 
 print(add(5, 8, 4, 6, 5, 2, 3))
 
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # print(type(kwargs))
+    # print(kwargs["add"])
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(5, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get('seats')
+my_car = Car(make="Nissan", model="GTR")
+print(my_car.color)
+input(">")
+
+
+
