3e52d1ea204318d8dcb9daaa7562b1ff990fa090
mehdi <m@gmail.com>
2023-04-07 23:28:43 +0430

    Create Rendering HTML/Static Files and Using Website Templates

---
+++ day56/main.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+        
+def is_authenticated_decoreator(funcation):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            funcation(args[0])
+    return wrapper
+
+@is_authenticated_decoreator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post ")
+
+new_user = User("angela")
+new_user.is_logged_in = True
+create_blog_post(new_user)
