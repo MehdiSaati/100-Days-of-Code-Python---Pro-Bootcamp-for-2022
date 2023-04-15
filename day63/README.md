79fd703ed3f6206e0cf36f2646ede09b3f9a0846
mehdi <m@gmail.com>
2023-04-15 12:05:30 +0430

    Create Databases and with SQLite and SQLAlchemy

---
+++ day63/README.md
@@ -0,0 +1,2 @@
+
+![day63](https://user-images.githubusercontent.com/98851253/162035331-cefbd488-261c-4897-9bbd-0ece40617ebb.gif)
---
+++ day63/book-rating/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+
+app = Flask(__name__)
+
+all_books=[]
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', books=all_books)
+
+@app.route('/add', methods=["GET","POST"])
+def add():
+    if request.method == "POST":
+        new_book =     {
+        "title": request.form["title"],
+        "author": request.form["author"],
+        "rating": request.form["rating"],
+        }
+        all_books.append(new_book)  
+        return redirect(url_for('home'))
+
+    return render_template('add.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
